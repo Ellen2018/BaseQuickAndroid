4d762f2e5c17d72fcd440c02e1b4106f6b276bcf
Ellen2018 <1961075191@qq.com>
2019-03-17 11:20:59 +0800

    BitmapUtils预计封装

--- app/src/main/java/com/ellen/basequickandroid/util/BitmapUtils.java
+++ app/src/main/java/com/ellen/basequickandroid/util/BitmapUtils.java
@@ -1,7 +1,8 @@
 package com.ellen.basequickandroid.util;
 
 /**
- * Bitmap在Android中图片编程需要接触的类
+ * Bitmap是在Android中图片编程需要接触的类，不仅仅是封装一些工具，还要将Bitmap一些难点
+ * 操作封装在里面,比如：加水印，旋转，透明的,饱和度d等等。
 */
 public class BitmapUtils {
 }
