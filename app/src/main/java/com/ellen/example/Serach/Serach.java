afa0df70092cf2e1a133db4d3c1f65720641cb51
Ellen2018 <1961075191@qq.com>
2019-04-24 18:14:28 +0800

    提交排序和查找的案例代码.

--- app/src/main/java/com/ellen/example/Serach/Serach.java
+++ app/src/main/java/com/ellen/example/Serach/Serach.java
@@ -2,4 +2,39 @@ package com.ellen.example.Serach;
 
 //查找算法归类
 public class Serach {
+
+    /*
+     * 循环实现二分查找算法arr 已排好序的数组x 需要查找的数-1 无法查到数据
+     */
+    public static int binarySearch(int[] arr, int x) {
+        int low = 0;
+        int high = arr.length-1;
+        while(low <= high) {
+            int middle = (low + high)/2;
+            if(x == arr[middle]) {
+                return middle;
+            }else if(x <arr[middle]) {
+                high = middle - 1;
+            }else {
+                low = middle + 1;
+            }
+        }
+        return -1;
+    }
+    
+    //递归实现二分查找
+    public static int binarySearch(int[] dataset,int data,int beginIndex,int endIndex){
+        int midIndex = (beginIndex+endIndex)/2;
+        if(data <dataset[beginIndex]||data>dataset[endIndex]||beginIndex>endIndex){
+            return -1;
+        }
+        if(data <dataset[midIndex]){
+            return binarySearch(dataset,data,beginIndex,midIndex-1);
+        }else if(data>dataset[midIndex]){
+            return binarySearch(dataset,data,midIndex+1,endIndex);
+        }else {
+            return midIndex;
+        }
+    }
+
 }
--- app/src/main/java/com/ellen/example/Sort/Sort.java
+++ app/src/main/java/com/ellen/example/Sort/Sort.java
@@ -3,9 +3,32 @@ package com.ellen.example.Sort;
 //排序算法归类
 public class Sort {
 
-    //冒泡排序
+    //冒泡排序范例
     public static void maoPaoSort(int[] array){
+        for(int i=0;i<array.length;i++) {
+            for(int j=i+1;j<array.length;j++) {
+                if(array[i]>array[j]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
 
+    //选择排序范例
+    public static void chooseSort(int[] array){
+        for(int i=0;i<array.length;i++) {
+            int tem = i;
+            for(int j=i;j<array.length;j++) {
+                if(array[j] < array[tem]) {
+                    tem = j;
+                }
+            }
+            int temp1 = array[i];
+            array[i] = array[tem];
+            array[tem] = temp1;
+        }
     }
 
 }
