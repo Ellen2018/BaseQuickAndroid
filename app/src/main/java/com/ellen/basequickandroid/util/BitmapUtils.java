19c5895bb366271e57a886f112697f589be5067e
Ellen2018 <1961075191@qq.com>
2019-04-26 17:04:21 +0800

    封装了Bitmap有关的工具类BitmapUtils

--- app/src/main/java/com/ellen/basequickandroid/util/BitmapUtils.java
+++ app/src/main/java/com/ellen/basequickandroid/util/BitmapUtils.java
@@ -1,8 +1,153 @@
 package com.ellen.basequickandroid.util;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.PixelFormat;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Build;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
- * Bitmap是在Android中图片编程需要接触的类，不仅仅是封装一些工具，还要将Bitmap一些难点
- * 操作封装在里面,比如：加水印，旋转，透明的,饱和度d等等。
+ *
+ * 1.将Drawable转化为Bitmap
+ * 2.获取到纯色Bitmap
+ *
 */
 public class BitmapUtils {
+
+    //Drawable -> Bitmap
+    public static Bitmap drawableToBitmap(Drawable drawable) {
+        // 取 drawable 的长宽
+        int w = drawable.getIntrinsicWidth();
+        int h = drawable.getIntrinsicHeight();
+        // 取 drawable 的颜色格式
+        Bitmap.Config config = drawable.getOpacity() != PixelFormat.OPAQUE ? Bitmap.Config.ARGB_8888
+                : Bitmap.Config.RGB_565;
+        // 建立对应 bitmap
+        Bitmap bitmap = Bitmap.createBitmap(w, h, config);
+        // 建立对应 bitmap 的画布
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, w, h);
+        // 把 drawable 内容画到画布中
+        drawable.draw(canvas);
+        return bitmap;
+    }
+
+    //获取到纯色Bitmap
+    public static Bitmap getColorBitmap(int width,int height,String colorString){
+        Bitmap bitmap = Bitmap.createBitmap(width, height,
+                Bitmap.Config.ARGB_8888);
+        bitmap.eraseColor(Color.parseColor(colorString));//填充颜色
+        return bitmap;
+    }
+
+    //通过本地地址获取到Bitmap
+    public static Bitmap getSampledBitmap(String filePath, int reqWidth, int reqHeight) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+        BitmapFactory.decodeFile(filePath, options);
+        int inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+        options.inSampleSize = inSampleSize;
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeFile(filePath, options);
+    }
+
+    public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) >= reqHeight
+                    && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+
+    //保存Bitmap到本地
+    public static String saveBitmap(Bitmap bm, String filePath) {
+        File f = new File(filePath);
+        if (f.exists()) {
+            f.delete();
+        }
+        try {
+            FileOutputStream out = new FileOutputStream(f);
+            bm.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.flush();
+            out.close();
+            return f.getPath();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // System.out.println("保存文件--->" + f.getAbsolutePath());
+        return filePath;
+    }
+
+    public static Bitmap getThumbnailByPath(String path, int size, Context context) throws IOException {
+        Uri userPickedUri = Uri.fromFile(new File(path));
+        return getThumbnailByUri(userPickedUri,size,context);
+    }
+
+    //得到本地图片对应的缩图
+    public static Bitmap getThumbnailByUri(Uri uri, int size, Context context) throws IOException {
+        InputStream input = context.getContentResolver().openInputStream(uri);
+        //配置BitmapFactory.Options，inJustDecodeBounds设为true，以获取图片的宽高
+        BitmapFactory.Options onlyBoundsOptions = new BitmapFactory.Options();
+        onlyBoundsOptions.inJustDecodeBounds = true;
+        onlyBoundsOptions.inDither=true;//optional
+        onlyBoundsOptions.inPreferredConfig=Bitmap.Config.ARGB_8888;//optional
+
+        //计算inSampleSize缩放比例
+        BitmapFactory.decodeStream(input, null, onlyBoundsOptions);
+        input.close();
+        if ((onlyBoundsOptions.outWidth == -1) || (onlyBoundsOptions.outHeight == -1))
+            return null;
+        int originalSize = (onlyBoundsOptions.outHeight > onlyBoundsOptions.outWidth) ? onlyBoundsOptions.outHeight : onlyBoundsOptions.outWidth;
+        double ratio = (originalSize > size) ? (originalSize / size) : 1.0;
+        //获取到缩放比例后，再次设置BitmapFactory.Options，获取图片缩略图
+        BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
+        bitmapOptions.inSampleSize = getPowerOfTwoForSampleRatio(ratio);
+        bitmapOptions.inDither=true;//optional
+        bitmapOptions.inPreferredConfig=Bitmap.Config.ARGB_8888;//optional
+        input = context.getContentResolver().openInputStream(uri);
+        Bitmap bitmap = BitmapFactory.decodeStream(input, null, bitmapOptions);
+        input.close();
+        return bitmap;
+    }
+
+    /**
+     * 将double的比例采用近似值的方式转为int
+     * @param ratio
+     * @return
+     */
+    private static int getPowerOfTwoForSampleRatio(double ratio){
+        int k = Integer.highestOneBit((int)Math.floor(ratio));
+        if(k==0) return 1;
+        else return k;
+    }
+
 }
